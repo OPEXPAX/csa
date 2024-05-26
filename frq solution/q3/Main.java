66596229f92ede9559c0075294820e8588ac9284
OPEXPAX <maxw6913@nycstudents.net>
2024-05-26 13:47:02 -0400

    Update Main.java

--- frq solution/q3/Main.java
+++ frq solution/q3/Main.java
@@ -1,1 +1,35 @@
+import java.util.ArrayList;
+public class Main{
+  public static void main(String[] args){
+    ArrayList<String> list1 = new ArrayList<String>();
+    list1.add("an"):
+    list1.add("band);
+    list1.add("band);
+    list1.add("abandon");
+    ArrayList<String>list2 = new ArrayList<String>();
+    list2.add("to");
+    list2.add("too");
+    list2.add("stool");
+    list2.add("tools");
+    WordChecker test1 = new WordChecker(list1);
+    WordChecker test2 = new WordChecker(list2);
 
+    System.out.println(test1.isWordChain());
+    System.out.println(test2.isWordChain());
+
+    ArrayList<String> list3 = new ArrayList<String>();
+    list3.add("catch");
+    list3.add("bobcat");
+    list3.add("catchacat");
+    list3.add("cat");
+    list3.add("at");
+    WordChecker test3 = new WordChecker(list3);
+    System.out.println(test3.createList("cat"));
+    System.out.println(test3.createList("catch"));
+    System.out.println(test3.createList("dog));
+
+
+
+      
+  }
+}
