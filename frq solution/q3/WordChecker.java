71b658d20e7afb45da03e0b60eb85a8effce5301
OPEXPAX <maxw6913@nycstudents.net>
2024-05-26 14:04:20 -0400

    Update WordChecker.java

--- frq solution/q3/WordChecker.java
+++ frq solution/q3/WordChecker.java
@@ -1,1 +1,36 @@
+import java.util.*;
+public class WordChecker {
+  private ArrayList<String> wordList;
+  public WordChecker(Array<String> words){
+    wordList = words;
+  }
 
+  public boolean isWordChain(){
+    for(int i=1; i<wordList.size(); i++){
+      String currentWord = wordList.get(i);
+      String previousWord = word.List.get(i-1);
+
+      boolean found = false;
+      for(int j = 0; j<= currentWord.length() - previousWord.length(); j++) {
+        if(currentWord.substring(j,j+previousWord.length()).equals(previousWord)){
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        return false;
+      }
+    }
+    return true;
+  }
+  public ArrayList<String> createList(String target){
+    ArrayList<String> result = new ArrayList<String>();
+    for(String word: wordList){
+      if(word.iindexOf(target)==0){
+        String modifiedWord = word.substring(target.length());
+        result.add(modifiedWord);
+      }
+    }
+    return result;
+  }
+}
