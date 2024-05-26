ff44b3aeec2da0eba3dc4c66858ef2a8bdc86b98
OPEXPAX <maxw6913@nycstudents.net>
2024-05-26 13:01:18 -0400

    Update Main.java

--- frq solution/q1/Main.java
+++ frq solution/q1/Main.java
@@ -1,1 +1,7 @@
-public static 
+public class Main {
+  public static void main(String[] args){
+
+    Feeder feeder = new Feeder(1000);
+    System.out.println(feeder.simulateManyDays(10,100));
+  }
+}
