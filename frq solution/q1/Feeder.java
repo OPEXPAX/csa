96a23fefe630ce9c6dd282326260183d996c5afd
OPEXPAX <maxw6913@nycstudents.net>
2024-05-26 13:09:03 -0400

    Update Feeder.java

--- frq solution/q1/Feeder.java
+++ frq solution/q1/Feeder.java
@@ -1,2 +1,38 @@
-public Feeder{
+public class Feeder{
+  private int currentFood;
+
+  public Feeder(int food){
+    currentFood = food;
+  }
+  public void simulateOneDay(int numBirds){
+    boolean normal = (int)(Math.random()*100+1) <= 95;
+    if(normal){
+      int gramsEaten = (int)(Math.random()*(50-10+1)+10);
+      int totalEaten = gramsEaten * numBirds;
+
+      if(totalEaten > currentFood){
+        currentFood = 0;
+      } else{
+        currentFood -= totalEaten;
+      }
+    }
+      public int getCurrentFood(){
+        return currentFood;
+      }
+  }
+public int simulateManyDays(int numBirds, int numDays){
+  int day = 0;
+  while(currentFood > 0 && day < numDays){
+    day++
+      simulateOneDay(numBirds);
+    System.out.println();
+    System.out.println("food left " + getCurrentFood());
+  }
+  System.out.print("days : ");
+  System.out.println();
+  return day;
+}
+
+  
+}
   
